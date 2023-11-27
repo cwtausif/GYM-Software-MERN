95b0bae4ae0d0852b6c268b2cff6c98918c7ed1a
Tausif <cwtausif@gmail.com>
2023-11-27 14:09:10 +0500

    Navbar, Top Bar and dashboard work

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,12 +1,48 @@
 // frontend/src/components/TopBar.js
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Menu, Notifications, AccountCircle, ExitToApp } from '@mui/icons-material';
 
-const TopBar = () => {
+const TopBar = ({ toggleLeftNavBar }) => {
+  const [leftNavBarOpen, setLeftNavBarOpen] = useState(true);
+
+  const handleToggleLeftNavBar = () => {
+    setLeftNavBarOpen(!leftNavBarOpen);
+    toggleLeftNavBar(!leftNavBarOpen);
+  };
+
   return (
-    <div>
-      <h2>Top Bar</h2>
-      {/* Add your top bar content, links, or buttons here */}
-    </div>
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleToggleLeftNavBar}
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Top Bar
+        </Typography>
+        <div>
+          {/* Notifications Icon */}
+          <IconButton color="inherit">
+            <Notifications />
+          </IconButton>
+          
+          {/* Profile Icon - You can replace 'user.jpg' with the actual path to the user's profile picture */}
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+
+          {/* Logout Icon */}
+          <IconButton color="inherit">
+            <ExitToApp />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
